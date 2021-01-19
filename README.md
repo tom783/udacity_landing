e9c9d8967fb8710f5172a71a88fa24d63570b0d9
hellground123 <dlgudwns783@gmail.com>
2021-01-19 22:52:40 +0900

    [update] apply feedback

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # udacity project
 
 ## This project will be expanded to a private project.
+
+---
+
+- JavaScript Design Pattern
+  - I tried to be grouped by function.
+  - I wanted to reduce repetition.
+
+```javascript
+init(); // I tried to prepare all the functions as a function.
+
+setFunction(), getFunction(); // get, set pattern.
+
+bindEvents(); // Tie up the event at once.
+```
--- js/app.js
+++ js/app.js
@@ -11,8 +11,13 @@ const navItems = [
     text: "Search",
     link: "searching-section",
   },
+  {
+    text: "contact",
+    link: "contact-section",
+  },
 ];
 
+// js init
 function init() {
   const $nav = document.querySelector("ul.nav");
   const $bannerSection = document.querySelector("#banner-section");
@@ -27,9 +32,11 @@ function init() {
   const $introTextAnimationFade = document.querySelector(
     "#intro-section .sub-text"
   );
+  const $searchingSection = document.querySelector("#searching-section");
   const $searchBar = document.querySelector(".search-bar");
   const $searching = document.querySelector(".search input");
-  const $searchingButton = document.querySelector("#searching");
+
+  const $testSection = document.querySelector("#contact-section");
 
   // create elements
   function _createElements() {
@@ -47,15 +54,13 @@ function init() {
     });
   }
 
+  // set scroll move function
   function setScrollMove() {
     const $navItems = $nav.querySelectorAll(".nav-item");
     $navItems.forEach((item) => {
       item.addEventListener("click", (e) => {
         e.preventDefault();
-        document
-          .querySelectorAll(".nav-item")
-          .forEach((i) => i.classList.remove("target"));
-        e.target.parentNode.classList.add("target");
+
         const target = e.target.getAttribute("data-link");
         const targetSection = document.querySelector(`#${target}`);
         targetSection.scrollIntoView({
@@ -66,30 +71,78 @@ function init() {
     });
   }
 
+  // set scroll effect function
   function setScrollAnimation() {
     let timerInstance = null;
 
+    //scroll css animation effect
+    function scrollAnimation() {
+      if (
+        window.scrollY >=
+        $introSection.offsetTop - $introSection.offsetHeight / 2
+      ) {
+        $introTextAnimationSlide.classList.add("animation-slide-leftright");
+        $introTextAnimationFade.classList.add("fadeIn");
+      } else {
+        $introTextAnimationSlide.classList.remove("animation-slide-leftright");
+        $introTextAnimationFade.classList.remove("fadeIn");
+      }
+    }
+
+    // scroll anchor
+    function scrollAnchor() {
+      const bannerSection = $bannerSection.getBoundingClientRect();
+      const introSection = $introSection.getBoundingClientRect();
+      const searchingSection = $searchingSection.getBoundingClientRect();
+      const testSection = $testSection.getBoundingClientRect();
+      document
+        .querySelectorAll(".nav-item")
+        .forEach((i) => i.classList.remove("target"));
+      if (
+        bannerSection.top <= 50 &&
+        bannerSection.bottom > bannerSection.height / 2
+      ) {
+        $nav
+          .querySelector('[data-link="banner-section"]')
+          .parentNode.classList.add("target");
+      } else if (
+        introSection.top <= 50 &&
+        introSection.bottom > introSection.height / 2
+      ) {
+        $nav
+          .querySelector('[data-link="intro-section"]')
+          .parentNode.classList.add("target");
+      } else if (
+        searchingSection.top <= 50 &&
+        searchingSection.bottom > searchingSection.height / 2
+      ) {
+        $nav
+          .querySelector('[data-link="searching-section"]')
+          .parentNode.classList.add("target");
+      } else if (
+        testSection.top <= 50 &&
+        testSection.bottom > testSection.height / 2
+      ) {
+        $nav
+          .querySelector('[data-link="contact-section"]')
+          .parentNode.classList.add("target");
+      }
+    }
+
+    // binding scroll event
     document.addEventListener("scroll", (e) => {
       if (!timerInstance) {
         timerInstance = setTimeout(function () {
           timerInstance = null;
-          if (
-            window.scrollY >=
-            $introSection.offsetTop - $bannerSection.offsetHeight / 2
-          ) {
-            $introTextAnimationSlide.classList.add("animation-slide-leftright");
-            $introTextAnimationFade.classList.add("fadeIn");
-          } else {
-            $introTextAnimationSlide.classList.remove(
-              "animation-slide-leftright"
-            );
-            $introTextAnimationFade.classList.remove("fadeIn");
-          }
+
+          scrollAnimation();
+          scrollAnchor();
         }, 200);
       }
     });
   }
 
+  // set search-input example list
   function setInputSearching() {
     let timerInstance = null;
     const listFrag = document.createDocumentFragment();
@@ -119,7 +172,7 @@ function init() {
   }
 
   // setting events
-  function setEvents() {
+  function bindEvents() {
     $bannerVideoCover.addEventListener("click", (e) => {
       $bannerVideoCover.style.display = "none";
       $bannerVideo.play();
@@ -135,7 +188,7 @@ function init() {
   }
 
   _createElements();
-  setEvents();
+  bindEvents();
 }
 
 init();
